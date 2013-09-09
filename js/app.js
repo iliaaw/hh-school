7d18548ab1de3ce84b1bda8a652e800b3f6b3213
Ilya <ilyahohryakov@gmail.com>
2013-09-10 01:20:33 +0400

    Interface improvements

--- js/app.js
+++ js/app.js
@@ -1,13 +1,18 @@
 function getDayOfWeek(date) {
-    var dayName = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота']
-    return dayName[date.getDay()]
+    var dayNames = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота']
+    return dayNames[date.getDay()]
+}
+
+function getNameOfMonth(date) {
+    var monthNames = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь']
+    return monthNames[date.getMonth()]
 }
 
 function myGetDay(date) {
     return date.getDay() == 0 ? 6 : date.getDay() - 1
 }
 
-function buildCell(date, firstRow) {
+function renderTableCell(date, firstRow) {
     var cellText = date.getDate()
     if (firstRow) {
         cellText = getDayOfWeek(date) + ', ' +cellText
@@ -15,26 +20,27 @@ function buildCell(date, firstRow) {
     return $('<td></td>').html(cellText)
 }
 
-function renderMonth(date) {
+function renderCalendar(date) {
     var firstDayOfMonth = new Date(date.getFullYear(), date.getMonth(), 1)
     var dayOfWeek = myGetDay(firstDayOfMonth)
     var currentDate = new Date(firstDayOfMonth)
     currentDate.setDate(currentDate.getDate() - dayOfWeek)
 
-    var tableRow = null;
-    var firstRow = true;
+    var tableRow = null
+    var firstRow = true
     var calendar = $('.calendar')
+    calendar.empty()
     while (true) {
         if (tableRow == null) {
             tableRow = $('<tr></tr>')
         }
-        tableRow.append(buildCell(currentDate, firstRow))
+        tableRow.append(renderTableCell(currentDate, firstRow))
         if (tableRow.children('td').length == 7) {
             calendar.append(tableRow)
             tableRow = null
             firstRow = false
             if (currentDate.getMonth() != date.getMonth()) {
-                break;
+                break
             }
         }
         currentDate.setDate(currentDate.getDate() + 1)
@@ -44,8 +50,43 @@ function renderMonth(date) {
     }
 }
 
+function renderCurrentMonth(date) {
+    $('.current-month').html([getNameOfMonth(date), date.getFullYear()].join(' '))
+}
+
+function bindTableHandlers() {
+    $('td').click(function(event) {
+        $('td').removeClass('current')
+        $(this).addClass('current')
+    })
+}
+
+function bindButtonHandlers() {
+    $('.button-left').click(function(event) {
+        _globalDate.setMonth(_globalDate.getMonth() - 1)
+        renderPage(_globalDate)
+    })
+
+    $('.button-right').click(function(event) {
+        _globalDate.setMonth(_globalDate.getMonth() + 1)
+        renderPage(_globalDate)
+    })
+
+
+    $('.button-today').click(function(event) {
+        _globalDate = new Date()
+        renderPage(_globalDate)
+    })
+}
+
+function renderPage(date) {
+    renderCalendar(date)
+    renderCurrentMonth(date)
+    bindTableHandlers()
+}
+
 $(document).ready(function() {
-    today = new Date()
-    today.setMonth(8)
-    renderMonth(today)
+    bindButtonHandlers()
+    _globalDate = new Date()
+    renderPage(_globalDate)
 })
